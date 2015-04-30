219b6c4242cd9dbacdea780ddf7075f0ee253e83
Hunter Braun <hunter.braun@gmail.com>
2015-04-30 15:47:28 -0500

    Add initial user input script

--- http-status.rb
+++ http-status.rb
@@ -399,3 +399,8 @@ statuses = [
     status_description: 'This status code is not specified in any RFCs, but is used by Microsoft HTTP proxies to signal a network connect timeout behind the proxy to a client in front of the proxy.'
   }
 ]
+
+print 'Enter HTTP status code: '
+code_from_user = gets.chomp
+status = statuses.select { |status| status[:status_code] == code_from_user }
+puts status
